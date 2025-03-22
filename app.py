78a4f3d92dcbbc973d168be25bef762c9406f06f
Aditya-k-Sharma <104820746+Aditya-k-Sharma@users.noreply.github.com>
2025-03-22 12:11:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+import uvicorn
+import sys
+import os
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+from fastapi.responses import Response
+from textSummarizer.pipeline.prediction import PredictionPipeline
+
+
+
+import nest_asyncio
+from pyngrok import ngrok
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+text:str = "What is Text Summarization?"
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+
+
+@app.get("/train")
+async def training():
+    try:
+        os.system("python main.py")
+        return Response("Training successful !!")
+
+    except Exception as e:
+        return Response(f"Error Occurred! {e}")
+    
+
+
+
+@app.post("/predict")
+async def predict_route(text):
+    try:
+
+        obj = PredictionPipeline()
+        text = obj.predict(text)
+        return text
+    except Exception as e:
+        raise e
+    
+
+if __name__=="__main__":
+       ngrok_tunnel = ngrok.connect(3000)
+       print('Public URL:', ngrok_tunnel.public_url)
+       nest_asyncio.apply()
+       uvicorn.run(app, port=3000)
